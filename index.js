c9b3d0155888f3fe1307378e8d6ef236ea23bd9f
lộc nguyễn <locflute@gmail.com>
2019-08-21 13:35:10 +0700

    fix 9h mới chạy v4

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ var request = require('request');
 
 
 let day = moment().add(1, 'd').format("YYYY-MM-DD");
-     datcom =async ()=>{ 
+     datcom =async() =>{ 
     if(new Date().getDay() === 5) {
         day = moment().add(3, 'd').format("YYYY-MM-DD");
     } else {
